69589c852760ef7cfe6836d2390827cf69f00a2c
Roma Korinenko <65089062+romakorinenko@users.noreply.github.com>
2025-09-17 08:13:41 +0300

    hw06: done pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	stagesChannels := make([]Bi, len(stages))
+	for i := 0; i < len(stages); i++ {
+		stagesChannels[i] = make(Bi)
+	}
+	subscribe(in, done, stages[0], stagesChannels[0])
+	for i := 1; i < len(stages); i++ {
+		subscribe(stagesChannels[i-1], done, stages[i], stagesChannels[i])
+	}
+	return stagesChannels[len(stages)-1]
+}
+
+func subscribe(in In, done In, stage Stage, stageChannel Bi) {
+	outChannel := stage(in)
+	go func() {
+		defer close(stageChannel)
+		for i := range outChannel {
+			select {
+			case <-done:
+				go func() {
+					for i := range outChannel {
+						_ = i
+					}
+				}()
+				return
+			case stageChannel <- i:
+			}
+		}
+	}()
 }
