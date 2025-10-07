21936e1da93703ea1c827047cdd69d0d9137991a
Roma Korinenko <65089062+romakorinenko@users.noreply.github.com>
2025-10-07 20:31:06 +0300

    hw07: done task

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,56 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	testCases := []struct {
+		name   string
+		offset int64
+		limit  int64
+		file   string
+	}{
+		{"out_offset0_limit0", 0, 0, "testdata/out_offset0_limit0.txt"},
+		{"out_offset0_limit10", 0, 10, "testdata/out_offset0_limit10.txt"},
+		{"out_offset0_limit1000", 0, 1000, "testdata/out_offset0_limit1000.txt"},
+		{"out_offset0_limit10000", 0, 10000, "testdata/out_offset0_limit10000.txt"},
+		{"out_offset100_limit1000", 100, 1000, "testdata/out_offset100_limit1000.txt"},
+		{"out_offset6000_limit1000", 6000, 1000, "testdata/out_offset6000_limit1000.txt"},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			out := "out.txt"
+			err := Copy("testdata/input.txt", out, testCase.offset, testCase.limit)
+			require.NoError(t, err)
+
+			actual, err := os.ReadFile(out)
+			require.NoError(t, err)
+			expected, err := os.ReadFile(testCase.file)
+			require.NoError(t, err)
+			require.Equal(t, expected, actual)
+
+			err = os.Remove(out)
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestCopy_ErrOffsetExceedsFileSize(t *testing.T) {
+	err := Copy("testdata/out_offset0_limit1000.txt", "out.txt", 99999999999999, 10)
+	require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+}
+
+func TestCopy_ErrNotExist(t *testing.T) {
+	err := Copy("notExist.txt", "out.txt", 0, 0)
+	require.ErrorIs(t, err, os.ErrNotExist)
+}
+
+func TestCopy_ErrUnsupportedFile(t *testing.T) {
+	dir := t.TempDir()
+	err := Copy(dir, "out.txt", 0, 0)
+	require.ErrorIs(t, err, ErrUnsupportedFile)
 }
