ab48c10d309aa65d87cb703ea883f47a0ae3ea91
Roma Korinenko <65089062+romakorinenko@users.noreply.github.com>
2025-10-27 17:18:38 +0300

    hw08: envdir tool finish

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,67 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("directory is not exists", func(t *testing.T) {
+		_, err := ReadDir("dir_is_not_exists")
+		require.Error(t, err)
+	})
+
+	t.Run("empty directory", func(t *testing.T) {
+		dir := "testdata/empty"
+
+		err := os.Mkdir(dir, 0o777)
+		require.Nil(t, err)
+		defer func() {
+			err = os.Remove(dir)
+			require.NoError(t, err)
+		}()
+
+		env, err := ReadDir(dir)
+		require.NoError(t, err)
+		require.Equal(t, Environment{}, env)
+	})
+
+	t.Run("success case", func(t *testing.T) {
+		expected := Environment{
+			"BAR":   {"bar", false},
+			"UNSET": {"", true},
+			"EMPTY": {"", false},
+			"FOO":   {"   foo\nwith new line", false},
+			"HELLO": {"\"hello\"", false},
+		}
+
+		actual, err := ReadDir("testdata/env")
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+	})
+
+	t.Run("incorrect filename", func(t *testing.T) {
+		dir := "testdata/invalid_env"
+
+		err := os.Mkdir(dir, os.FileMode(0o755))
+		require.NoError(t, err)
+		defer func() {
+			err = os.RemoveAll(dir)
+			require.NoError(t, err)
+		}()
+
+		f, err := os.Create(path.Join(dir, "NAME=INVALID"))
+		require.Nil(t, err)
+		defer func() {
+			err = f.Close()
+			require.NoError(t, err)
+		}()
+
+		env, err := ReadDir(dir)
+		require.Zero(t, env)
+		require.Error(t, err)
+	})
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) < 1 {
+		return 1
+	}
+
+	envs := make([]string, 0)
+	for k, v := range env {
+		if _, ok := os.LookupEnv(k); ok {
+			if err := os.Unsetenv(k); err != nil {
+				return 1
+			}
+		}
+		if !v.NeedRemove {
+			varString := fmt.Sprintf("%s=%s", k, v.Value)
+			envs = append(envs, varString)
+		}
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	command.Env = append(os.Environ(), envs...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	if err := command.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return command.ProcessState.ExitCode()
 }
