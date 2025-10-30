c4c2d51956bd6a481b4abef74a09d3a5dc539da7
Roma Korinenko <65089062+romakorinenko@users.noreply.github.com>
2025-10-30 17:05:07 +0300

    Revert "hw08: envdir tool finish"
    This reverts commit 24d95903295815acd30dd88f583f1acd7612caac.

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
+import "testing"
 
 func TestRunCmd(t *testing.T) {
-	t.Run("read", func(t *testing.T) {
-		expected := Environment{
-			"BAR":   {"bar", false},
-			"UNSET": {"", true},
-			"EMPTY": {"", false},
-			"FOO":   {"   foo\nwith new line", false},
-			"HELLO": {"\"hello\"", false},
-		}
-
-		env, err := ReadDir("./testdata/env")
-		require.NoError(t, err)
-		require.Equal(t, expected, env)
-	})
-
-	t.Run("path not found", func(t *testing.T) {
-		var expected Environment
-
-		env, err := ReadDir("./testdata/no_file")
-		require.Error(t, err)
-		require.Equal(t, expected, env)
-	})
+	// Place your code here
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"log"
-	"os"
-)
-
 func main() {
-	if len(os.Args) < 3 {
-		log.Fatal("two arguments required")
-	}
-
-	env, err := ReadDir(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	code := RunCmd(os.Args[2:], env)
-	os.Exit(code)
+	// Place your code here.
 }
