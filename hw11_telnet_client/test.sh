c2e64ee4a1d18106a9fdced6cadbe0674df64b5c
Roma Korinenko <65089062+romakorinenko@users.noreply.github.com>
2025-10-31 11:14:08 +0300

    hw11: correct test.sh

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -3,11 +3,11 @@ set -xeuo pipefail
 
 go build -o go-telnet
 
-(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
+(printf "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
 NC_PID=$!
 
 sleep 1
-(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >/tmp/telnet.out &
+(printf "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >/tmp/telnet.out &
 TL_PID=$!
 
 sleep 5
